39e23d67ddc0b7524cdcc488f0b51804b6b93bc8
Rajpreet0 <87982307+Rajpreet0@users.noreply.github.com>
2022-08-07 21:11:55 +0200

    Update Hero.js

--- components/home/hero/Hero.js
+++ components/home/hero/Hero.js
@@ -2,13 +2,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 import HeroBackground from '../../../assets/images/home/hero_background.png';
-import Fade from 'react-reveal/Fade';
+
 
 function Hero() {
   return (
     <div className='min-h-screen pt-36 font-merri'>
       <div className='flex flex-col items-center w-full justify-around md:flex-row'> 
-     <Fade left> 
+   
         <div className='p-4 md:pl-12 md:text-left'>
             <h1 className='text-4xl md:text-5xl' style={{color: '#0F3D3E'}}>Lorem ipsum dolor<br/>sit amet,</h1>
             <h2 className='mt-4 text-xl' style={{color: '#876445'}}>Lorem ipsum dolor sit amet, consetetur sadipscing elit <br/> consetetur sadipscing sit amet </h2>
@@ -22,15 +22,14 @@ function Hero() {
             </Link>
           </div>
         </div>
-      </Fade>
-       <Fade right> 
+
         <div className='md:pt-12'>
         <Image
             src={HeroBackground}
             alt=''
            />
         </div>
-      </Fade>
+
       </div>
     </div>
   )
